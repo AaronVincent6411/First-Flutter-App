85a8a8835eb4bfd3187fc5522257ba1279a0c4c6
Aaron P Laju <aaronvincent6411@users.noreply.github.com>
2023-03-16 13:32:24 -0400

    added background color
    Signed-off-by: Aaron P Laju <aaronvincent6411@users.noreply.github.com>

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'First Flutter App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),
       ),
@@ -81,7 +81,10 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Text(pair.asLowerCase),
